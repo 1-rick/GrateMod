23a31785c451c3caf5af2c74714c08785dddbf76
1-rick <r.a.cooley@gmail.com>
2015-11-07 00:22:38 -0600

    add a logger
    Instead of writing to System.out

--- src/main/java/GrateMod/GrateMod.java
+++ src/main/java/GrateMod/GrateMod.java
@@ -6,27 +6,35 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.init.Blocks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = GrateMod.MODID, version = GrateMod.VERSION)
 public class GrateMod {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "0.0.1";
+    public static final String MODID = "GrateMod";
+    public static final String VERSION = "${version}";
+    public static final Logger logger = LogManager.getLogger(MODID);
+
+    public GrateMod() {
+        logger.info("Grate!");
+    }
 
     @EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
-        // some example code
-        System.out.println("GrateMod PreInit");
+    public void preInit(FMLPreInitializationEvent event) {
+        // item+block init+registering
+        // config handling
+        logger.info("GrateMod preInit");
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // some example code
-        System.out.println("GrateMod Init");
+        // proxy, tileentity, entity, gui, packet registration
+        logger.info("GrateMod init");
     }
 
     @EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
+    public void postInit(FMLPostInitializationEvent event) {
         // some example code
-        System.out.println("GrateMod PostInit");
+        logger.info("GrateMod postInit");
     }
 }
